a75088a55b4ceeefad98051b7c463752e7560a63
Andrew Shaffer <andrew@weo.io>
2016-03-17 19:09:26 -0700

    add IE <= 10 polyfill

--- test/index.js
+++ test/index.js
@@ -15,6 +15,18 @@ test('should set prototype', function (t) {
     return 'next'
   }}
   setProto(proto, obj)
+  t.equal(obj.__proto__, proto)
   t.equal(obj.next(), 'next')
+  t.end()
+})
+
+test('ie <= 10 polyfill', function (t) {
+  var obj = Object.create(null)
+  var proto = {t: 1}
+
+  setProto(proto, obj)
+  t.equal(obj.t, 1)
+  t.deepEqual(Object.keys(obj), ['t'])
+
   t.end()
 })
